8db013d9236f189e6cdaea4476753f00d11dd00c
Kevin3874 <kzhang77@jh.edu>
2023-03-26 19:52:50 -0400

    api added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,121 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useState } from 'react';
+import {GoogleLogin, GoogleLogout} from 'react-google-login';
+const { google } = require('googleapis');
+const CLIENT_ID = '342594362833-03cf0lfba838qp2i4c02kl8vb5o24sef.apps.googleusercontent.com';
+const CLIENT_SECRET = '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'
+const REDIRECT_URI = 'https://developers.google.com/oauthplayground';
+const REFRESH_TOKEN = '1//04UqiO2roSeTRCgYIARAAGAQSNwF-L9IrDjLxgzrG8JaoPuRrbHjk5kFXD28eHCanSCeDM2xMcWjwiKaU5Ctz2dcMsbgtO5WK2e8'
+const oAuth2Client = new google.auth.OAuth2(
+  CLIENT_ID,
+  CLIENT_SECRET,
+  REDIRECT_URI
+);
+const [accessToken, setAccessToken] = useState<string>('');
+oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
+
+const handleLoginSuccess = (response: any) => {
+  setAccessToken(response.accessToken);
+  oAuth2Client.setCredentials({access_token: response.accessToken});
+};
+
+const handleLogoutSuccess = () => {
+  setAccessToken('');
+};
+
+const listFiles = async () => {
+  const drive = google.drive({version: 'v3', auth: accessToken});
+  const response = await drive.files.list();
+  console.log(response.data.files);
+};
+
+const downloadFile = async (fileId: string) => {
+  const drive = google.drive({version: 'v3', auth: accessToken});
+  const response = await drive.files.get({fileId, alt: 'media'}, {responseType: 'blob'});
+  const url = window.URL.createObjectURL(new Blob([response.data]));
+  const link = document.createElement('a');
+  link.href = url;
+  link.setAttribute('download', `${fileId}.pdf`);
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+};
+
+const listFileUsers = async (fileId: string) => {
+  const drive = google.drive({version: 'v3', auth: accessToken});
+  const response = await drive.permissions.list({fileId});
+  console.log(response.data.permissions);
+};
+
+const createPushNotification = async (fileId: string) => {
+  const drive = google.drive({version: 'v3', auth: accessToken});
+  const channel = {
+    id: 'unique-channel-id',
+    type: 'web_hook',
+    address: 'http://localhost:3000/api/notifications'
+  };
+  const {data: {expiration, resourceId}} = await drive.files.watch({
+    fileId,
+    requestBody: channel
+  });
+  console.log(`Watch channel created for file ${fileId}, expiration: ${expiration}, resourceId: ${resourceId}`);
+};
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className="App">
+      {!accessToken ? (
+        <GoogleLogin
+          clientId={CLIENT_ID}
+          buttonText="Login with Google"
+          onSuccess={handleLoginSuccess}
+          onFailure={handleLoginFailure}
+          cookiePolicy="single_host_origin"
+          responseType="code,token"
+        />
+      ) : (
+        <>
+          <GoogleLogout
+            clientId={CLIENT_ID}
+            buttonText="Logout"
+            onLogoutSuccess={handleLogoutSuccess}
+          />
+          <div>
+            <button onClick={listFiles}>List Files</button>
+          </div>
+          {files && (
+            <div>
+              <h3>Files:</h3>
+              <ul>
+                {files.map((file) => (
+                  <li key={file.id}>
+                    {file.name}
+                    <button onClick={() => downloadFile(file.id)}>
+                      Download
+                    </button>
+                    <button onClick={() => listFileUsers(file.id)}>
+                      List Users
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+          {selectedFile && (
+            <div>
+              <h3>Users with access to {selectedFile.name}:</h3>
+              {fileUsers && (
+                <ul>
+                  {fileUsers.map((user) => (
+                    <li key={user.emailAddress}>{user.emailAddress}</li>
+                  ))}
+                </ul>
+              )}
+            </div>
+          )}
+        </>
+      )}
+    </div>
+  );
 }
